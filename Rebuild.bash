a029d1f13ca1f9d2c4efa39cf115767219e6af43
Daniel Kehsler <daniel.kehsler@oceansonics.com>
2021-08-19 19:02:56 -0300

    Add some utility scripts

---
+++ Rebuild.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -u
+set -e
+set -x
+
+# Clean build artefacts
+time make clean
+
+# Update the OpenWrt feeds (https://openwrt.org/docs/guide-developer/feeds):
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+# Expand the diffconfig into a buildroot config file:
+cp diffconfig .config
+make defconfig
+
+# Make the SD card images and SDK:
+time make download
+time make -j8
---
+++ WriteImage.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -u
+set -e
+set -x
+
+# Use the following command to search for image files:
+# find ./bin/targets/bcm27xx/bcm2711/ -type f -name '*-ext4-sysupgrade.img.gz'
+
+# Set variables:
+DISK_PATH='/dev/sd???'
+IMAGE_ARCHIVE_PATH='./bin/targets/bcm27xx/bcm2711/openwrt-21.02-snapshot-????????'
+
+# Write the image:
+sudo dd status=progress conv=fsync bs=2M count=1 "if=/dev/zero" "of=${DISK_PATH}"
+gunzip -c "${IMAGE_ARCHIVE_PATH}" | sudo dd status=progress conv=fsync bs=2M "of=${DISK_PATH}"
+sync
